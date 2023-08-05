587475ea8e8208f68bd5a4f58cb671f4f534261b
Juan Cruz Lima Caramico <juan.mdq22@gmail.com>
2023-08-05 17:44:50 -0300

    fix cors policy

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ app.use(cors())
 app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://mooviedb.netlify.app");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
